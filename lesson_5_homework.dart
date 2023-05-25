18c400cc592aaf112fdb59f809a6518e97bd2999
Timur2009KG <asanalievt93@gmail.com>
2023-05-25 13:54:22 +0300

    first commit

---
+++ lesson_5_homework.dart
@@ -0,0 +1,96 @@
+void main() {
+  int bild = 4;
+
+  switch (bild) {
+    case 1:
+      print(
+          "пойти в вшколу, прибрать дома, СВИДАНИЕ, приготовить ужин, арабские курсы, и спать");
+      break;
+
+    case 2:
+      print(
+          "почтиать книги, сделать уроки , позавтракать, прибрать дома, школа, обед, Flutter, и спать");
+      break;
+
+    case 3:
+      print(
+          "пойти в вшколу, прибрать дома, приготовить ужин, арабские курсы, и спать");
+      break;
+
+    case 4:
+      print(
+          "почтиать книги, сделать уроки , позавтракать, прибрать дома, школа, обед, Flutter, и спать");
+      break;
+    case 5:
+      print(
+          "пойти в вшколу, прибрать дома, дородой, приготовить ужин, арабские курсы, и спать");
+      break;
+    case 5:
+      print("пойти в школу, купить продукты,сделать,уроки, флаттер");
+      break;
+    case 6:
+      print(
+          "пойти в вшколу, прибрать дома, СВИДАНИЕ, приготовить ужин, арабские курсы, и спать");
+      break;
+    case 7:
+      print("спать");
+      break;
+    default:
+      print("out of range");
+  }
+
+  //  num seazon = 1;
+
+  //  switch (seazon) {
+  //    case 1:
+  //      print("snow");
+  //      break;
+
+  //    case 2:
+  //      print("spring");
+  //      break;
+
+  //    case 3:
+  //      print("sunny");
+  //      break;
+
+  //    case 4:
+  //      print("outumn");
+  //      break;
+
+  //    default:
+  //      print("error");
+  //  }
+
+  // String lang = "en";
+
+  // if (lang == 'ru') {
+  //   print("понедельник вторник среда четверг пятница суббота воскресенье");
+  // } else if (lang == "en") {
+  //   print("monday tuesday wednesday thirsdey friday saturday sunday");
+  // } else {
+  //   print("error");
+  // }
+
+  // switch (lang) {
+  //   case 'ru':
+  //     print("понедельник вторник среда четверг пятница суббота воскресенье");
+  //     break;
+
+  //   case "en":
+  //     print("monday tuesday wednesday thirsdey friday saturday sunday");
+  //     break;
+
+  //   default:
+  //     print("error");
+  // }
+
+  // int a = 10;
+
+  // switch(a){
+  //   case 10:
+  //   print("value is 10");
+  //   default:
+  //   print("out of range");
+  // }
+}
